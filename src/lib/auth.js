db3714be6711e610ac250ea8ca671290d8f1b0c3
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-08-29 10:39:31 +0600

    authentication setup done

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -12,6 +12,10 @@ export const auth = betterAuth({
     client,
   }),
 
+  baseURL: process.env.BETTER_AUTH_URL,
+
+  secret: process.env.BETTER_AUTH_SECRET,
+
   emailAndPassword: {
     enabled: true,
   },
